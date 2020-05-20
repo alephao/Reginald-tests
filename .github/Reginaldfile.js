b1eae73c6176cb70466b645de2e05ba9930e7e4f
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-05-20 12:13:21 +1000

    Update reginaldfile

--- .github/Reginaldfile.js
+++ .github/Reginaldfile.js
@@ -1,6 +1,12 @@
-if (!/\[[A-Z]+-[0-9]+\]\s/.test(reginald.pr.title)) {
-  reginald.error("Title should start with the ticket nuber e.g.: [JIRA-123]")
-}
+reginald.message("This is a message")
+reginald.message("Another message")
+reginald.warning("This is a warning")
+reginald.warning("One more warning for you!")
+reginald.error("This is an error, and it fails the check!")
+
+// if (!/\[[A-Z]+-[0-9]+\]\s/.test(reginald.pr.title)) {
+//   reginald.error("Title should start with the ticket nuber e.g.: [JIRA-123]")
+// }
 
 // if (reginald.git.modifiedFiles.some(value => value.endsWith('Reginaldfile.js'))) {
 //   reginald.warning("Reginaldfile was modified");
